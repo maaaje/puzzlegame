ef9a72269534ad958154bb522d1ac3c17fc762aa
Marvin <matrap97@gmail.com>
2020-10-21 20:20:37 +0200

    finished level-changing system.

--- src/Game.gd
+++ src/Game.gd
@@ -6,11 +6,16 @@ onready var scene_changer = get_node("/root/SceneChanger")
 
 var game_ended = false
 
+func _ready():
+	scene_changer.connect("scene_changed", self, "on_Scene_changed")
+
 func _process(_delta):
 	spots_node = get_node("Level").get_child(0).get_node("Spots")
 	current_level = get_node("Level").get_child(0)
 	var spot_count = spots_node.get_child_count()
 	
+	if Input.is_action_just_pressed("restart"):
+		SceneChanger.change_scene(current_level.level_number)
 	
 	if !game_ended:	
 		for i in spots_node.get_children():
@@ -22,3 +27,6 @@ func _process(_delta):
 		#var current_level = get_node("Level").get_child(0)
 		#get_node("Level").remove_child(current_level)
 		#get_node("Level").add_child(next_level.instance())
+
+func on_Scene_changed():
+	game_ended = false
